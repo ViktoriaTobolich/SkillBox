0a9b54d20b801edaef6a0d3d508047ad8bd13743
vtabolich <v.tabolich@reply.com>
2022-03-04 08:05:49 +0300

    task

--- src/ex.ts
+++ src/ex.ts
@@ -1,3 +1,6 @@
+console.log("ex.ts!!!!!");
+
+
 function concatStrings(str1: string, str2: string): string{
   return str1 + str2;
 }
@@ -11,5 +14,18 @@ interface myHometask {
 const MyHometask: myHometask = {
   howIDoIt: "I Do It Wel",
   simeArray: ["string one", "string two", 42],
-  withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
+  withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }]
 }
+
+//3
+interface MyArray<T> {
+	[N: number]: T
+
+ reduce<T>( fn:(el: T[], num: T) => T): T
+ //map<U>(fn: (el: T) => U): U[]
+}
+
+const testArray: MyArray<number> = [1,2,3];
+const initialValue = 0;
+
+testArray.reduce((accumulator, value) => accumulator + value, initialValue);
